bc450d0b8ee09776d3bcdb6ace748f8d2e04559e
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2025-10-07 12:39:21 +0800

    Add checkstyle test

---
+++ test.py
@@ -0,0 +1,14 @@
+def func():
+    print("Hello, World!")
+    return 42
+
+if __name__ == "__main__":
+    func()
+
+def test():
+    a= 1
+    b= 2
+    assert a+b  ==3
+    l = [0,1,2,3,4,5]
+    g =l[1: 4]
+
