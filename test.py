3d6055ae0b0254c269a2c667f9552759ae3646a6
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2025-10-07 13:25:51 +0800

    Add style bug example

--- test.py
+++ test.py
@@ -11,4 +11,15 @@ def test():
     assert a+b  ==3
     l = [0,1,2,3,4,5]
     g =l[1: 4]
+    if a >  0:
+        print("a is positive")
+    elif a > 0:
+        print("a is zero")
+    else:
+        print("a is negative")
 
+    for i in range(5):
+        print(i)
+
+    while ( a < 5 ):
+        a += 1
