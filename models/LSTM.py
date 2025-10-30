42af0e55e700761ddeb883fb2a087b7de0a20711
jhqiu21 <jhqiu06@outlook.com>
2025-10-30 22:20:08 +0800

    run baseline

--- models/LSTM.py
+++ models/LSTM.py
@@ -17,8 +17,8 @@ import flax.linen as nn
 
 class CharLSTM(nn.Module):
     vocab_size: int
-    hidden_size: int = 512
-    n_layers: int = 2
+    hidden_size: int = 256
+    n_layers: int = 4
     dropout: float = 0.1
     max_len: int = 128
     tie_weights: bool = False  # if True: use E^T for projection
